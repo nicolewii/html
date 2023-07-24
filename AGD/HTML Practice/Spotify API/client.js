f2bdc88fc05acd3b8e27b414d78d21057c7ff19e
Nicole <108346371+nicolewii@users.noreply.github.com>
2023-07-24 10:12:16 -0400

    Add files via upload

---
+++ AGD/HTML Practice/Spotify API/client.js
@@ -0,0 +1,19 @@
+var client_id = '813f9f0e06914fc0abfe16476cbf1f06';
+var client_secret = '59330af3d22a40929b1730650fa9cb39';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
